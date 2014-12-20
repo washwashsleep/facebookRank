b8e8782c60ee8bbdce3ba2a61099504f643db3c1
Simon Sun <blackghost0118@gmail.com>
2014-12-20 18:32:28 +0800

    view init

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ var controllers = require('./controllers');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('swig').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, '/public')));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- controllers/test/homePage.js
+++ controllers/test/homePage.js
@@ -1,3 +1,4 @@
 module.exports = function (req, res, next){
-	res.send('This is new Home Page QOO');
+	// res.send('This is new Home Page QOO');
+	res.render('test.html', {test: 'QQ'});
 };
