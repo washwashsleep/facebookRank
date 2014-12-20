130438893458c0f4cfeb4c5c3e1d5994af81aee8
Simon Sun <blackghost0118@gmail.com>
2014-12-21 05:37:07 +0800

    fans list page data

--- controllers/fans/pageList.js
+++ controllers/fans/pageList.js
@@ -1,7 +1,40 @@
+var _ = require('lodash');
+var async = require('async');
+var request = require('request');
+
+var db = require('../../models');
+
 module.exports = function (req, res, next){
-	if(!req.session.user || !req.session.user.userId){
-		return res.redirect('/auth/login');
-	}
+	
+	async.waterfall([
+		function (cb){
+			if(!req.session.user || !req.session.user.userId){
+				return res.redirect('/auth/login');
+			}
+			
+			cb();
+		},
+		
+		function (cb){
+			db.userFansList.findOne({ userId: req.session.user.userId }, cb);
+		},
+		
+		function (userFansInfo, cb){
+			if(!userFansInfo){
+				return cb(null, []);
+			}
+			
+			db.fans.find({ fanId: { $in: userFansInfo.list } }, cb);
+		}
+	],
+	function (err, userFansList){
+		if(err){
+			return next(err);
+		}
+		// res.send(userFansList);
+		res.render('fansList.html', { userFansList: userFansList });
+	});
+
 	// need fansList.html
-	res.send('fans list page .html');
+	// res.send('fans list page .html');
 };
