590c60cb76b231bd700df01e9327aa020308b39f
Simon Sun <blackghost0118@gmail.com>
2014-12-21 16:29:36 +0800

    QQ

---
+++ controllers/fans/pageShow.js
@@ -0,0 +1,10 @@
+var _ = require('lodash');
+var async = require('async');
+var request = require('request');
+
+var db = require('../../models');
+
+module.exports = function (req, res, next){
+	
+	// render fanShow.html
+};
