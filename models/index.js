7913a479cea812519e45dbfde50e9942aafe810c
Simon Sun <blackghost0118@gmail.com>
2014-12-21 16:26:37 +0800

    cronjob 完成

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
-var mongojs = require('mongojs')
-, db = mongojs('mongodb://128.199.223.114:27017/facebookRank')
-, users = db.collection('users')
-, userFansList = db.collection('userFansList')
-, fans = db.collection('fans');
+var mongojs = require('mongojs'),
+	db = mongojs('mongodb://128.199.223.114:27017/facebookRank'),
+	users = db.collection('users'),
+	userFansList = db.collection('userFansList'),
+	fans = db.collection('fans'),
+	fansUpdateInfo = db.collection('fansUpdateInfo');
 
 module.exports = {
 	users: users,
 	userFansList: userFansList,
-	fans: fans
+	fans: fans,
+	fansUpdateInfo: fansUpdateInfo
 };
