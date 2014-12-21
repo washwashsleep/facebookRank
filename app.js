83bb7f6444a7b7d88a7c049ce64b8798224d5c97
webber0928 <bird23074035@gmail.com>
2014-12-21 09:14:30 +0800

    優化

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('cookie-session');
-
-var controllers = require('./controllers');
-
-var app = express();
+var express = require('express')
+, path = require('path')
+, favicon = require('serve-favicon')
+, logger = require('morgan')
+, cookieParser = require('cookie-parser')
+, bodyParser = require('body-parser')
+, session = require('cookie-session')
+, controllers = require('./controllers')
+, app = express();
 
 // view engine setup
 app.engine('html', require('swig').renderFile);
@@ -16,20 +14,16 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/public')));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({
-    key: 'sessionId',
-    secret: 'session_cookie_secret+sdjfiawe958723',
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 60
-    }
+  key: 'sessionId',
+  secret: 'session_cookie_secret+sdjfiawe958723',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 60
+  }
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -41,27 +35,22 @@ app.use('/fans', controllers.fans);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -69,6 +58,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
-var mongojs = require('mongojs');
-var db = mongojs('mongodb://128.199.223.114:27017/facebookRank');
-var users = db.collection('users');
-var userFansList = db.collection('userFansList');
-var fans = db.collection('fans');
-
+var mongojs = require('mongojs')
+, db = mongojs('mongodb://128.199.223.114:27017/facebookRank')
+, users = db.collection('users')
+, userFansList = db.collection('userFansList')
+, fans = db.collection('fans');
 
 module.exports = {
 	users: users,
